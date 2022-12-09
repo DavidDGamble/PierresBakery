dce77ab624d67784c4b95c57810cd498402cbebc
DavidDGamble <daviddgamble@gmail.com>
2022-12-09 10:59:07 -0800

    Add test for PastryConstructor and skeleton code to Pastry.cs. Good fail.

---
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PierresBakery.Models;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry(5);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+  }
+}
