844ffea008e483ad0e48161a43495c51f4738022
DavidDGamble <daviddgamble@gmail.com>
2022-12-09 16:23:06 -0800

    Refactor code in Bread.cs and Pastry.cs.

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -2,24 +2,24 @@ namespace PierresBakery.Models
 {
   public class Pastry
   {
-    private int PastryTotal;
+    private int _pastryTotal;
     private int _totalCost { get; set; }
 
     public Pastry(int pastryTotal)
     {
-      PastryTotal = pastryTotal;
+      _pastryTotal = pastryTotal;
       _totalCost = 0;
     }
 
-    public int GetPastryTotal() { return PastryTotal; }
+    public int GetPastryTotal() { return _pastryTotal; }
     public int GetTotalCost() { return _totalCost; }
 
     public void CalcTotal()
     {
-      _totalCost = PastryTotal * 2;
-      if (PastryTotal > 2)
+      _totalCost = _pastryTotal * 2;
+      if (_pastryTotal > 2)
       {
-        int freeNum = PastryTotal / 3;
+        int freeNum = _pastryTotal / 3;
         _totalCost -= freeNum;
       }
     }
